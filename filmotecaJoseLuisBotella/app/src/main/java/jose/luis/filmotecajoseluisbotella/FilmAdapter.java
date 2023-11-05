1337738e3781bedb51d956dc127023948f0facf0
joseluisbotelladevesa <144933281+joseluisbotelladevesa@users.noreply.github.com>
2023-11-05 17:08:51 +0100

    sexto commit

--- filmotecaJoseLuisBotella/app/src/main/java/jose/luis/filmotecajoseluisbotella/FilmAdapter.java
+++ filmotecaJoseLuisBotella/app/src/main/java/jose/luis/filmotecajoseluisbotella/FilmAdapter.java
@@ -17,7 +17,7 @@ public class FilmAdapter extends ArrayAdapter <Film>{
 
     private Context context;
     private int resource;
-    private ArrayList<Film> misPeliculas = null;
+    private ArrayList<Film> misPeliculas;
 
     public FilmAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Film> misPeliculas) {
         super(context, resource, misPeliculas);
@@ -30,26 +30,42 @@ public class FilmAdapter extends ArrayAdapter <Film>{
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflador=LayoutInflater.from(this.getContext());
+        LayoutInflater inflador = LayoutInflater.from(this.getContext());
 
-        View mifila=inflador.inflate(resource, parent, false);
+        View mifila = inflador.inflate(resource, parent, false);
 
         Film film = misPeliculas.get(position);
 
-        TextView titulo= mifila.findViewById(R.id.txtFilm);
-        TextView director=mifila.findViewById(R.id.directordata);
-        TextView anyo=mifila.findViewById(R.id.anyodata);
-        TextView tipo=mifila.findViewById(R.id.genero);
-        ImageView imgFilm=mifila.findViewById(R.id.imgFilm);
+        TextView titulo = mifila.findViewById(R.id.txtFilm);
+        TextView director = mifila.findViewById(R.id.txtdirector);
+        ImageView imgFilm = mifila.findViewById(R.id.imgFilm);
+
+        titulo.setText(misPeliculas.get(position).getTitle());
+        director.setText(misPeliculas.get(position).getDirector());
+        imgFilm.setImageResource(misPeliculas.get(position).getImageResId());
 
 
-     /*   String nombre=misPeliculas.get(position).getComments();
+        TextView titulodata = mifila.findViewById(R.id.titulodata);
+        TextView directordata = mifila.findViewById(R.id.directordata);
+        ImageView caratuladata = mifila.findViewById(R.id.caratuladata);
+
+        //titulodata.setText(misPeliculas.get(position).getTitle());
+        /*directordata.setText(misPeliculas.get(position).getDirector());
+        caratuladata.setImageResource(misPeliculas.get(position).getImageResId());
+
+        /*   String nombre=misPeliculas.get(position).getComments();
         txtCiudad.setText(nombre);
         String descripcion=misPeliculas.get(position).getDescripcion();
         txtDescripcion.setText(descripcion);
         int idimagen=misPeliculas.get(position).getImagen();
         imgCiudad.setImageResource(idimagen);
-*/
+
+
+         */
         return mifila;
     }
-}
+        private String getGenreText(int genre) {
+            return "Género: " + genre;
+        }
+
+    }
